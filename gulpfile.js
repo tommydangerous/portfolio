1eeff46d3753ee64131b72655dc0be8f1db0f7c3
Tommy Dang <quantumventuress@gmail.com>
2015-03-05 17:05:33 -0800

    added compression

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 
-var uglify = require('gulp-uglify');
+var uglify    = require('gulp-uglify');
+var uglifycss = require('gulp-uglifycss');
 
 gulp.task('compress', function() {
+  gulp.src('./public/css/development/application.css')
+    .pipe(uglifycss())
+    .pipe(gulp.dest('./public/css/production'));
+
   gulp.src('./public/js/development/bundle.js')
     .pipe(uglify())
-    .pipe(gulp.dest('./public/js/production'))
+    .pipe(gulp.dest('./public/js/production'));
 });
