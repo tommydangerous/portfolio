7f51af5b0e54d017196fee16613ffa7715b0f36d
Tommy Dang <quantumventuress@gmail.com>
2015-03-06 12:44:24 -0800

    restored pages controller back to normal

--- app/express/controllers/pages_controller.js
+++ app/express/controllers/pages_controller.js
@@ -1,10 +1,6 @@
 exports.index = function(req, res) {
-  // res.render('pages/index', {
-  //   title: 'Dangerous',
-  //   environment: process.env.NODE_ENV
-  // });
-  res.render('pages/test', {
-    title: 'Test',
-    environment: process.env.NODE_ENV
+  res.render('pages/index', {
+    environment: process.env.NODE_ENV,
+    title: 'Dangerous'
   });
 };
