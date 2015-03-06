7b173a7e7206ba711a79f1a647e60e80b98d03d4
Tommy Dang <quantumventuress@gmail.com>
2015-03-06 11:37:17 -0800

    removed logger from server.js

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.set('views', './app/express/views');
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 
-app.use(express.logger());
 app.use(express.static(__dirname + '/public'));
 
 require('./app/express/routers/router')(app);
