8737a42de261f7b703c457a35251350d22751eb5
Tommy Dang <quantumventuress@gmail.com>
2015-03-06 12:24:53 -0800

    render test in pages controller

--- app/express/controllers/pages_controller.js
+++ app/express/controllers/pages_controller.js
@@ -1,6 +1,7 @@
 exports.index = function(req, res) {
-  res.render('pages/index', {
-    title: 'Dangerous',
-    environment: process.env.NODE_ENV
-  });
+  // res.render('pages/index', {
+  //   title: 'Dangerous',
+  //   environment: process.env.NODE_ENV
+  // });
+  res.render('pages/test');
 };
--- app/express/routers/router.js
+++ app/express/routers/router.js
@@ -1,11 +1,8 @@
 var pagesController = require('../controllers/pages_controller');
 
 module.exports = function(app) {
-  app.route('/').get(function(req, res) {
-    res.send('yo');
-  });
-  // app.route('/').get(pagesController.index);
-  // app.route('/about').get(pagesController.index);
-  // app.route('/contact').get(pagesController.index);
-  // app.route('/portfolio').get(pagesController.index);
+  app.route('/').get(pagesController.index);
+  app.route('/about').get(pagesController.index);
+  app.route('/contact').get(pagesController.index);
+  app.route('/portfolio').get(pagesController.index);
 };
